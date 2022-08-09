41d8fcdee55be69a3a337a7fb7e3aeba41f4243a
Aly <alex.mitan@yahoo.com>
2022-08-09 18:38:33 +0300

    title, page loader comments

--- loadForPages.sh
+++ loadForPages.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 
+npm run build
+
+# force copy build to docs
 cp -r dist/* docs/
-# rm -rf dist/*
+rm -rf dist/*
+
+# change "/" to "./" for sources in index
 sed -i 's/\"\//\"\.\//g' docs/index.html
